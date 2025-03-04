3b76c7da5adb08893bfde61320d917d4e1349f65
up240667 <up240667@alumnos.upa.edu.mx>
2025-03-04 09:36:38 -0600

    Abanse 4/mar/25

---
+++ Ejercicio 8.py
@@ -0,0 +1,8 @@
+#Ejercicio 8
+#Calculate the slope, x-intercept and y-intercept of y = 2x -2
+pendiente=2
+inty=-2
+intx=(pendiente/inty)
+print("la intx es:",intx)
+print("la pendiente es de:",pendiente)
+
