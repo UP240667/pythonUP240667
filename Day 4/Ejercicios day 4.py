22ccaffbfc38238d6c94662673e8e21a1beb0dc4
up240667 <up240667@alumnos.upa.edu.mx>
2025-03-05 10:33:29 -0600

    Avance day 4

---
+++ Day 4/Ejercicios day 4.py
@@ -0,0 +1,55 @@
+print("Ejercicio 1")
+#Concatene la cadena 'Thirty', 'Days', 'Of', 'Python' en una sola cadena, 'Thirty Days Of Python'
+primer_adena="Thirty"
+segunda_cadena=" Days "  #la concatenacion de cadenas es simplemente la unin de palbras mediante una suma de las mismas.
+tercera_cadena="of "
+cuata_cadena="Python"
+todas_las_cadenas=(primer_adena+segunda_cadena+tercera_cadena+cuata_cadena)
+print(todas_las_cadenas)
+
+print("Ejercicio 2")
+#Concatene la cadena 'Coding', 'For' , 'All' en una sola cadena, 'Coding For All'.
+C1="Coding "
+C2="For "
+C3="All"
+Concatenacion=(C1+C2+C3)
+print(Concatenacion)
+
+print("Ejercicio 3")
+#Declare una variable denominada company y asígnela a un valor inicial "Coding For All".
+nombre="Company"
+nombre="Coding For All"
+
+print("Ejercicio 4")
+#Imprime la variable company usando print().
+nombre="Company"
+nombre="Coding For All"
+print(nombre="Coding For All")
+
+print("Ejercicio 5")
+#Imprima la longitud de la cadena de la empresa utilizando el método len() y print().
+cadena="Coding For All"
+print(len(cadena))
+
+print("Ejercicio 6")
+#Cambie todos los caracteres a letras mayúsculas usando el método upper().
+Caracteres="Coding For All"
+print(Caracteres.upper())#EL METODO upper() se utilisa para hacer mayuscula una cadena.
+
+print("Ejercicio 7")
+#Cambie todos los caracteres a letras minúsculas usando el método lower().
+Caracteres="Coding For All"
+print(Caracteres.lower())#el metodo lower() hace todo lo contrario que upper()ya que este comberte toda la cadena en minusculas.
+
+print("Ejercicio 8")
+#Utilice los métodos capitalize(), title(), swapcase() para formatear el valor de la cadena Coding For All.
+cadena="Coding For All"
+print(cadena.capitalize()) #capitalize(): Convierte el primer carácter de la cadena en letra mayúscula
+print(cadena.title()) #title(): Devuelve una cadena de título con mayúsculas y minúsculas
+print(cadena.swapcase()) #swapcase(): Convierte todos los caracteres en mayúsculas a minúsculas y todos los caracteres en minúsculas a caracteres en mayúsculas
+
+print("Ejercicio 9")
+#Corta la primera palabra de la cadena Coding For All.
+palabra="Coding For All"
+corte=palabra[6:]#con este [x:] podemos eliminar a antidad de palabras que queramos , osea esto dice que ba a iniciar de el numero x en adelante lo que quede atras no lo leera.
+print(corte)
---
+++ Day 4/Pruevas.py
@@ -0,0 +1,6 @@
+
+print("Ejercicio 9")
+#Corta la primera palabra de la cadena Coding For All.
+palabra="Coding For All"
+corte=palabra[6:]
+print(corte)
