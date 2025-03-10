6db6e86309aaa88e02e09bf9bb6fc9344cd4e68a
up240667 <up240667@alumnos.upa.edu.mx>
2025-03-09 21:33:33 -0600

    fin dia 4

--- Day 4/Ejercicios day 4.py
+++ Day 4/Ejercicios day 4.py
@@ -142,3 +142,98 @@ print(orac) #este metodo rindex nos sirve para verificar la ultima aparicion de 
 
 print("Ejercicio 25")
 #Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+oracionn="You cannot end a sentence with because because because is a conjuction"
+oracion_nueva=oracionn.replace("because because because", "")#este metodo .replace("") nos sirve para eliminar palabras o letras de una cadena
+print(oracion_nueva)
+
+print("Ejercicio 26")
+#Find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+sentence= 'You cannot end a sentence with because because because is a conjunction'
+because_in_sentence=sentence.find("because")
+print(because_in_sentence)#este metdo se utiliza para la ubicacion de una subcadena o mas bien una palabra dentro de una cadena
+
+print("Ejercicio 27")
+#Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+sentencee='You cannot end a sentence with because because because is a conjunction'
+because_3=sentencee.strip("because because because")# el metodo .strip()se utiliza para eliminar caracteres dentro de una cadena.
+print(because_3)
+
+print("Ejercicio 28")
+#Does ''Coding For All' start with a substring Coding?
+coding="Coding For All".startswith("Coding")#El metodo .startswith() se utiliza para verificar su una oracion o cadena comienza con una palabra o subcadena especifica.
+print(coding)
+
+print("Ejercicio 29")
+#Does 'Coding For All' end with a substring coding?
+codigo="Coding For All".endswith("coding")#Este .endswith() metodo funciona para verificar si una cadena termina con una sub cadena especifica osea lo contrario de .startswith()
+print(codigo)
+
+print("Ejercicio 30")
+#'   Coding For All      '  , remove the left and right trailing spaces in the given string.
+cadene='   Coding For All      '
+sin_espacios=cadene.strip()#el metodo strip() se utiliza para eliminar los espacios en una cadena
+print(sin_espacios)
+
+print("Ejercicio 31")
+#Which one of the following variables return True when we use the method isidentifier():
+#30DaysOfPython
+#thirty_days_of_python
+variable1="30DaysOfPython"
+variable2="thirty_days_of_python"
+print(variable1.isidentifier())
+print(variable2.isidentifier())#este metodo se utiliza para identificar si una cadena de texto es valida en python
+
+print("Ejercicio 32")
+#The following list contains the names of some of python libraries: ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']. Join the list with a hash with space string.
+librerias='Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon'
+resultado="#".join(librerias)#la funcion join funciona para concatenar(Unir) una lista de texto en una sola cadena.
+print(resultado)
+
+print("Ejercicio 33")
+#Use the new line escape sequence to separate the following sentences.
+#I am enjoying this challenge.
+#I just wonder what is next.
+oraciom="I am enjoying this challenge\n I just wonder what is next"#la operacion \n reprecenta un salto de linea
+print(oraciom)
+
+print("Ejercicio 34")
+#Use a tab escape sequence to write the following lines.
+#Name      Age     Country   City
+#Asabeneh  250     Finland   Helsinki
+print("Name/Asabeneh")
+print("Age/250")
+print("Country/Finland")
+print("City/Helsinki")
+
+print("Ejercicio 35")
+#Use the string formatting method to display the following:
+#radius = 10
+#area = 3.14 * radius ** 2
+#The area of a circle with radius 10 is 314 meters square.
+radio=15
+area=3.14*radio**2
+area_final="El area de el circulo con radio de %r is %a metros"%(radio,area)# aqui utilize los porcentajes para denominarlos como una variable para el radio y el area.
+print(area_final)
+
+print("Ejercicio 36")
+#Make the following using string formatting methods:
+#8 + 6 = 14
+#8 - 6 = 2
+#8 * 6 = 48
+#8 / 6 = 1.33
+#8 % 6 = 2
+#8 // 6 = 1
+#8 ** 6 = 262144
+a=10
+b=7
+print(f"{a}+{b}={a+b}")#la f permite la incorporacion de expreciones dentro de las cadenas de texto {expreciones}
+print(f"{a}-{b}={a-b}")
+print(f"{a}*{b}={a*b}")
+print(f"{a}/{b}={a/b}")
+print(f"{a}%{b}={a%b}")
+print(f"{a}//{b}={a//b}")
+print(f"{a}**{b}={a**b}")
+
+print("Final de el dia 4")
+
+
--- Day 4/Pruevas.py
+++ Day 4/Pruevas.py
@@ -1,7 +1,19 @@
 
-print("Ejercicio 24")
-#Use rindex to find the position of the last occurrence of the word because in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
-orac="You cannot end a sentence with because because because is a conjunction".rindex("because")
-print(orac)
-
-
+print("Ejercicio 36")
+#Make the following using string formatting methods:
+#8 + 6 = 14
+#8 - 6 = 2
+#8 * 6 = 48
+#8 / 6 = 1.33
+#8 % 6 = 2
+#8 // 6 = 1
+#8 ** 6 = 262144
+a=10
+b=7
+print(f"{a}+{b}={a+b}")#la f permite la incorporacion de expreciones dentro de las cadenas de texto {expreciones}
+print(f"{a}-{b}={a-b}")
+print(f"{a}*{b}={a*b}")
+print(f"{a}/{b}={a/b}")
+print(f"{a}%{b}={a%b}")
+print(f"{a}//{b}={a//b}")
+print(f"{a}**{b}={a**b}")
